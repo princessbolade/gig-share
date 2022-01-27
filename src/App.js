a6c848ef0805deaa003f5d9ca7b1510b871bbfcc
Princessbolade <mobolade.akinlua@outlook.com>
2022-01-27 13:30:22 +0100

    feat:fix overflow

--- src/App.js
+++ src/App.js
@@ -10,36 +10,36 @@ import hero3 from "./assets/hero3.svg";
 import hero1 from "./assets/hero1.svg";
 
 function App() {
-	return (
-		<Box bg="#f2f2f2" minH="100vh" w="100vw" overflowX="hidden" p="20px">
-			<Header />
-			<Process />
-			<Gig />
-			<Share />
-			<Footer />
-			<Image
-				bottom={"10%"}
-				right={"0"}
-				position="absolute"
-				src={Footer3}
-				borderLeftRadius="30px"
-			/>
-			<Image
-				top={"5%"}
-				right={"0"}
-				position="absolute"
-				src={hero3}
-				borderLeftRadius="30px"
-			/>
-			<Image
-				top={"5%"}
-				left={"0"}
-				position="absolute"
-				src={hero1}
-				borderRightRadius="30px"
-			/>
-		</Box>
-	);
+  return (
+    <Box bg="#f2f2f2" overflowX="hidden" p="20px">
+      <Header />
+      <Process />
+      <Gig />
+      <Share />
+      <Footer />
+      <Image
+        bottom={"10%"}
+        right={"0"}
+        position="absolute"
+        src={Footer3}
+        borderLeftRadius="30px"
+      />
+      <Image
+        top={"5%"}
+        right={"0"}
+        position="absolute"
+        src={hero3}
+        borderLeftRadius="30px"
+      />
+      <Image
+        top={"5%"}
+        left={"0"}
+        position="absolute"
+        src={hero1}
+        borderRightRadius="30px"
+      />
+    </Box>
+  );
 }
 
 export default App;
